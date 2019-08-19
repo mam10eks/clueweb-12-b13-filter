73a7345a08733aaed9d0fa2bce97fa08691204a9
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2019-08-19 07:12:58 +0200

    Add rocksdb

--- src/main/java/de/webis/clueweb_filter/Clueweb12B13Filter.java
+++ src/main/java/de/webis/clueweb_filter/Clueweb12B13Filter.java
@@ -1,31 +1,86 @@
 package de.webis.clueweb_filter;
 
+import java.io.File;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
 
 import lombok.SneakyThrows;
 
 public class Clueweb12B13Filter {
-	public static final Set<String> IDS = Collections.unmodifiableSet(loadIds());
 
+	private static final byte[] DUMMY_VALUE = toBytes("a");
+
+	private static final String DB_PATH = ".clueweb-12b13-ids-rocksdb";
+
+	private static final List<String> TEST_IDS = Collections.unmodifiableList(Arrays.asList(
+			"clueweb12-1914wb-28-24250", "clueweb12-0000tw-00-00013"
+		));
+	
+	private static final RocksDB ROCKS_DB = rocksDbWithIds();
+	
 	@SneakyThrows
-	private static Set<String> loadIds() {
-		InputStream resource = Clueweb12B13Filter.class.getResourceAsStream("../../../clueweb-12-b13-ids.txt");
-		Set<String> ret = new HashSet<>();
+	private static RocksDB rocksDbWithIds() {
+		RocksDB db;
 		
-		for(String id: IOUtils.readLines(resource, StandardCharsets.UTF_8)) {
-			ret.add(id);
+		if(dbExists()) {
+			db = createTemporaryRocksDb();
+			
+			if(TEST_IDS.stream().allMatch(id -> idIsClueweb12B13(db, id))) {
+				return db;
+			}
+			
+		}
+		else {
+			db = createTemporaryRocksDb();
 		}
 		
-		return ret;
+		List<String> ids = readAllIds();
+		
+		for (String id : ids) {
+			db.put(toBytes(id), DUMMY_VALUE);
+		}
+
+		return db;
+	}
+	
+	@SneakyThrows
+	public static List<String> readAllIds() {
+		InputStream resource = Clueweb12B13Filter.class.getResourceAsStream("../../../clueweb-12-b13-ids.txt");
+		return IOUtils.readLines(resource, StandardCharsets.UTF_8);
 	}
 
+	@SneakyThrows
+	private static RocksDB createTemporaryRocksDb() {
+		RocksDB.loadLibrary();
+		Options options = new Options();
+		options.setCreateIfMissing(Boolean.TRUE);
+		
+		return RocksDB.open(options, DB_PATH);
+	}
+
+	private static boolean dbExists() {
+		return new File(DB_PATH).exists()
+			&& new File(DB_PATH).list().length > 0;
+	}
+	
+	@SneakyThrows
 	public static boolean idIsClueweb12B13(String id) {
-		return IDS.contains(id);
+		return idIsClueweb12B13(ROCKS_DB, id);
+	}
+	
+	@SneakyThrows
+	private static boolean idIsClueweb12B13(RocksDB db, String id) {
+		return db.get(toBytes(id)) != null;
+	}
+
+	private static byte[] toBytes(String v) {
+		return v.getBytes(StandardCharsets.UTF_8);
 	}
 }
--- src/test/java/de/webis/clueweb_filter/Clueweb12B13FilterTest.java
+++ src/test/java/de/webis/clueweb_filter/Clueweb12B13FilterTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 public class Clueweb12B13FilterTest {
 	@Test
 	public void checkThatClueweb12B13HasFiftyMillionIds() {
-		long expected = 52000000l;
-		long actual = Clueweb12B13Filter.IDS.size();
+		long expected = 52343021l;
+		long actual = Clueweb12B13Filter.readAllIds().size();
 		
 		Assert.assertEquals(expected, actual);
 	}
 	
 	@Test
-	public void checkThatClueweb12B13HashIsCorrect() {
-		int expected = "adsasd".hashCode();
-		int actual = Clueweb12B13Filter.IDS.hashCode();
+	public void checkThatHashOfClueweb12B13Ids() {
+		long expected = 477663244;
+		long actual = Clueweb12B13Filter.readAllIds().hashCode();
 		
 		Assert.assertEquals(expected, actual);
 	}
@@ -28,9 +28,19 @@ public class Clueweb12B13FilterTest {
 	}
 	
 	@Test
+	public void checkTheFirstIdIsExisting() {
+		Assert.assertTrue(Clueweb12B13Filter.idIsClueweb12B13("clueweb12-0000tw-00-00013"));
+	}
+	
+	@Test
+	public void checkTheLastIdIsExisting() {
+		Assert.assertTrue(Clueweb12B13Filter.idIsClueweb12B13("clueweb12-1914wb-28-24250"));
+	}
+	
+	@Test
 	public void checkSomeNonExistingExampleIds() {
-		Assert.assertTrue(Clueweb12B13Filter.idIsClueweb12B13("cluseweb12-0000tw-00-00041"));
-		Assert.assertTrue(Clueweb12B13Filter.idIsClueweb12B13("clueweb12-19s14wb-28-24133"));
-		Assert.assertTrue(Clueweb12B13Filter.idIsClueweb12B13("clueweb12-1a914wb-28-24250"));
+		Assert.assertFalse(Clueweb12B13Filter.idIsClueweb12B13("cluseweb12-0000tw-00-00041"));
+		Assert.assertFalse(Clueweb12B13Filter.idIsClueweb12B13("clueweb12-19s14wb-28-24133"));
+		Assert.assertFalse(Clueweb12B13Filter.idIsClueweb12B13("clueweb12-1a914wb-28-24250"));
 	}
 }
